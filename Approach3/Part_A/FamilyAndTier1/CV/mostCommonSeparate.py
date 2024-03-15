7b47684ecf974e597f8c6e01e0d63ee74a505dda
Ali Ameli <aameli1376@gmail.com>
2024-03-15 14:38:51 -0230

    Update mostCommonSeparate.py

--- Approach3/Part_A/FamilyAndTier1/CV/mostCommonSeparate.py
+++ Approach3/Part_A/FamilyAndTier1/CV/mostCommonSeparate.py
@@ -28,8 +28,8 @@ if __name__ == "__main__":
     
     SNPsList = list(SNPsDic.keys())
     print(len(SNPsList))
-
-    path = '/scratch/fs2/usefi/pd/datasets/All_Aproaches/Approach3/Datasets/ID/'
+    # needs to be replaced by the path of files
+    path = ''
 
     print("current path of working space= " + path)
   
