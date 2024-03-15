fbd3e49bcb619f69cdc6cc766572b6ae61575d13
Ali Ameli <aameli1376@gmail.com>
2024-03-15 14:38:06 -0230

    Update loop_SVFS.py

--- Approach3/Part_A/FamilyAndTier1/SVFs/loop_SVFS.py
+++ Approach3/Part_A/FamilyAndTier1/SVFs/loop_SVFS.py
@@ -15,7 +15,8 @@ import csv
 def main():
 
     dataset_list = ['FamilyTier1(Merged)']
-    path = "/scratch/fs2/usefi/pd/datasets/All_Aproaches/Approach3/Datasets/"
+    # needs to be replaced by the path of files
+    path = ""
     # Parameters
     k = 50
     th_irr = 3
