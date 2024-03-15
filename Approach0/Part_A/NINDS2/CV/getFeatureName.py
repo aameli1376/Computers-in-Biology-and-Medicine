54f7b733076bfe9d710e2aa1e95a750ecf41e1de
Ali Ameli <aameli1376@gmail.com>
2024-03-15 14:12:53 -0230

    Update getFeatureName.py

--- Approach0/Part_A/NINDS2/CV/getFeatureName.py
+++ Approach0/Part_A/NINDS2/CV/getFeatureName.py
@@ -17,8 +17,8 @@ if __name__ == "__main__":
     
     SNPsList = list(SNPsDic.keys())
     print(len(SNPsList))
-
-    path = '/scratch/fs2/usefi/pd/datasets/All_Aproaches/Approach0/Datasets/'
+    # needs to be replaced by the path of files
+    path = ''
 
     print("current path of working space= " + path)
   
